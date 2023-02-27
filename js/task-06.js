f186d6855e406ee7e21dbf934a53158b4a5bb9ef
Oleh Protasevych <protasevicholeg@gmail.com>
2023-02-27 23:06:45 +0200

    Do task-06

---
+++ js/task-06.js
@@ -0,0 +1,17 @@
+const validationInputEl = document.querySelector('#validation-input');
+const dataLengthValue = Number(validationInputEl.dataset.length);
+
+const onInputBlur = event => {
+  if (event.currentTarget.value.length === dataLengthValue) {
+    validationInputEl.classList.add('valid');
+    validationInputEl.classList.remove('invalid');
+  } else if (event.currentTarget.value.length === 0) {
+    validationInputEl.classList.remove('valid');
+    validationInputEl.classList.remove('invalid');
+  } else {
+    validationInputEl.classList.add('invalid');
+    validationInputEl.classList.remove('valid');
+  }
+};
+
+validationInputEl.addEventListener('blur', onInputBlur);
