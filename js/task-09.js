6e045a2b584bd933fcad48935393866152927287
Oleh Protasevych <protasevicholeg@gmail.com>
2023-02-28 17:50:27 +0200

    Do task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widgetEl = document.querySelector('.widget');
+const colorEl = widgetEl.querySelector('.color');
+const changeColorButtonEl = widgetEl.querySelector('.change-color');
+
+changeColorButtonEl.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorEl.textContent = newColor;
+});
