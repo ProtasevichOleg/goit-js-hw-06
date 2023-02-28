3dc84e763a3534c06599c8a5422bf7dc15039b46
Oleh Protasevych <protasevicholeg@gmail.com>
2023-02-28 17:28:05 +0200

    Do task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginFormEl = document.querySelector('.login-form');
 
+const onSubmitBtnClick = event => {
+  event.preventDefault();
+
+  const { email, password } = loginFormEl.elements;
+
+  if (!email.value || !password.value) alert('Всі поля мають бути заповнені');
+
+  const user = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(user);
+  loginFormEl.reset();
+};
+
+loginFormEl.addEventListener('submit', onSubmitBtnClick);
