4e89080a38aa0da5922058b9b0ce6120c17e1191
Oleh Protasevych <protasevicholeg@gmail.com>
2023-03-01 16:23:07 +0200

    Rewrite branching on task-05 using switch with one extra case ' '

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,14 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 const onInputChange = event => {
-  nameOutput.textContent =
-    event.currentTarget.value === ''
-      ? 'Anonymous'
-      : event.currentTarget.value.trim();
+  switch (event.currentTarget.value.trim()) {
+    case '':
+    case ' ':
+      nameOutput.textContent = 'Anonymous';
+      break;
+    default:
+      nameOutput.textContent = event.currentTarget.value.trim();
+  }
 };
 
 nameInput.addEventListener('input', onInputChange);
